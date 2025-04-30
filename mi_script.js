cdc3c968091492cfcfbe8d7fdf5a5e0ce8e1ac28
paz449BpA <A100238449@my.uvm.edu.mx>
2025-04-30 13:15:19 -0600

    mi_script.js

---
+++ mi_script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.getElementById("miBoton");
+    boton.addEventListener("click", () => {
+        alert("¡Gracias por visitar nuestro proyecto sobre el reciclaje del papel!");
+    });
+});
